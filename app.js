3cb54d51355b518890ecfc33bb152d6d502fd380
Mehrsa <mhrs.project22@gmail.com>
2023-12-08 07:37:51 +0330

    show current weather data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const API_KEY = "422e21bdd1d866deee3cf5b15241194c";
 
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
+const weatherContainer = document.getElementById("weather");
 
 // get current weathers data url
 const getCurrentWeatherByName = async (city) => {
@@ -10,6 +11,27 @@ const getCurrentWeatherByName = async (city) => {
     const response = await fetch(url);
     const json = await response.json();
     return json;
+};
+
+// show weather data in site (current weather)
+const renderCurrentWeather = (data) => {
+console.log(data)
+
+    // if (!data) return;
+    const weatherJSX = `
+        <h1>${data.name}, ${data.sys.country}</h1>
+        <div id="main">
+            <img alt="weather icon" src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" />
+            <span>${data.weather[0].main}</span>
+            <p>${Math.round(data.main.temp)} °C</p>
+        </div>
+        <div id="info"> 
+            <p>Humidity: <span>${data.main.humidity}</span></p>
+            <p>Wind Speed: <span>${data.wind.speed} m/s</span></p>
+        </div>
+    `;
+
+    weatherContainer.innerHTML = weatherJSX; 
 };
 
 // get data by city name
@@ -21,7 +43,7 @@ const searchHandler = async () => {
     }
     
     const currentData = await getCurrentWeatherByName(cityName);
-    console.log(currentData)
+    renderCurrentWeather(currentData)
 };
 
 searchButton.addEventListener("click", searchHandler);
